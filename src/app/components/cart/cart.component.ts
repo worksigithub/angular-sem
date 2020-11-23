b2af6eba287f7f681b9d9ae9b7f68319b474e1f5
worksigithub <37853670+worksigithub@users.noreply.github.com>
2020-11-23 13:38:03 -0300

    Fix ruta src

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ProductoService } from 'src/app/services/producto.service';
+import { ProductoService } from '../../services/producto.service';
 import { Products } from '../products/products';
 
 @Component({
--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ProductoService } from 'src/app/services/producto.service';
+import { ProductoService } from '../..//services/producto.service';
 import { Products } from '../products/products';
 
 @Component({
